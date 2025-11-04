a1583f0c3ada95e2d7a43ea9ef779b05f18196b7
Diogo C. <kdc09272001@gmail.com>
2025-11-04 18:58:38 -0500

    Added classes

---
+++ game-10003-the-maze-game/Game.cs
@@ -0,0 +1,42 @@
+﻿// Include the namespaces (code libraries) you need below.
+using System;
+using System.IO;
+using System.Numerics;
+
+// The namespace your code is in.
+namespace MohawkGame2D
+{
+    /// <summary>
+    ///     Your game code goes inside this class!
+    /// </summary>
+    public class Game
+    {
+        // Place your variables here:
+        public bool onCollide = true;
+
+        /// <summary>
+        ///     Setup runs once before the game loop begins.
+        /// </summary>
+        public void Setup()
+        {
+            Window.SetTitle("The Maze Game");
+            Window.SetSize(800, 600);
+        }
+
+        /// <summary>
+        ///     Update runs every frame.
+        /// </summary>
+        public void Update()
+        {
+            if (onCollide)
+            {
+                
+            }
+            else
+            {
+
+            }
+        }
+    }
+
+}
---
+++ game-10003-the-maze-game/mazePath.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MohawkGame2D
+{
+    internal class mazePath
+    {
+
+        public void Setup()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        void ProcessCollision()
+        {
+
+        }
+
+    }
+}
