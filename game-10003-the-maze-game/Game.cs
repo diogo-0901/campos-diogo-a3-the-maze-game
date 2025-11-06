f239b76b57d246613b15e0d87366a271d49c9090
Diogo C. <kdc09272001@gmail.com>
2025-11-06 15:37:46 -0500

    Created level layout

--- game-10003-the-maze-game/Game.cs
+++ game-10003-the-maze-game/Game.cs
@@ -14,9 +14,27 @@ namespace MohawkGame2D
     {
         // Place your variables here:
         public bool onCollide = true;
-        mazeCursor Cursor = new mazeCursor(new Vector2(10, 10), new Vector2(10, 10));
+        mazeCursor Cursor = new mazeCursor(new Vector2(-1, -1), new Vector2(7, 7));
         mazeHitbox[] mazeWall = { 
-        new mazeHitbox(new Vector2(0, 0), new Vector2(200, 490), true, Color.Black)
+        new mazeHitbox(new Vector2(0, 0), new Vector2(400, 220), true, Color.Black),
+        new mazeHitbox(new Vector2(0, 220), new Vector2(100, 380), true, Color.Black),
+        new mazeHitbox(new Vector2(100, 550), new Vector2(700, 50), true, Color.Black),
+        new mazeHitbox(new Vector2(460, 0), new Vector2(800, 260), true, Color.Black),
+        new mazeHitbox(new Vector2(700, 420), new Vector2(800, 180), true, Color.Black),
+        new mazeHitbox(new Vector2(150, 260), new Vector2(800, 160), true, Color.Black),
+        new mazeHitbox(new Vector2(100, 470), new Vector2(560, 40), true, Color.Black),
+
+
+        new mazeHitbox(new Vector2(410, 230), new Vector2(30, 30), true, Color.Black),
+        new mazeHitbox(new Vector2(440, 120), new Vector2(30, 140), true, Color.Black),
+        new mazeHitbox(new Vector2(400, 180), new Vector2(30, 40), true, Color.Black),
+        new mazeHitbox(new Vector2(410, 140), new Vector2(30, 30), true, Color.Black),
+        new mazeHitbox(new Vector2(400, 120), new Vector2(30, 10), true, Color.Black),
+        new mazeHitbox(new Vector2(400, 40), new Vector2(10, 80), true, Color.Black),
+        new mazeHitbox(new Vector2(400, 0), new Vector2(60, 40), true, Color.Black),
+        new mazeHitbox(new Vector2(410, 40), new Vector2(50, 80), true, Color.Red),
+
+
         };
         /// <summary>
         ///     Setup runs once before the game loop begins.
@@ -25,7 +43,7 @@ namespace MohawkGame2D
         {
             Window.SetTitle("The Maze Game");
             Window.SetSize(800, 600);
-            Window.TargetFPS = 30;
+            Window.TargetFPS = 120;
         }
 
         /// <summary>
@@ -34,7 +52,7 @@ namespace MohawkGame2D
         public void Update()
         {
             Window.ClearBackground(Color.Cyan);
-            Cursor.Update();
+            Cursor.Update(mazeWall);
             for (int i = 0; i < mazeWall.Length; i++)
             {
                 mazeWall[i].Update();
--- game-10003-the-maze-game/mazeCursor.cs
+++ game-10003-the-maze-game/mazeCursor.cs
@@ -17,10 +17,11 @@ namespace MohawkGame2D
             this.size = size;
         }
 
-        public void Update()
+        public void Update(mazeHitbox[] mazeWall)
         {
             playerDraw();
             playerMovement();
+            collisionProcess(mazeWall);
         }
 
         public void playerDraw()
@@ -36,9 +37,39 @@ namespace MohawkGame2D
             pos = Input.GetMousePosition();
         }
 
-        public void collisionProcess()
+        public void collisionProcess(mazeHitbox[] mazeWall)
         {
+            for(int mazebox = 0; mazebox < mazeWall.Length; mazebox++)
+            {
+                mazeHitbox hitbox = mazeWall[mazebox];
+                float hitboxTop = hitbox.pos.Y;
+                float hitboxBottom = hitbox.pos.Y + hitbox.size.Y;
+                float hitboxLeft = hitbox.pos.X;
+                float hitboxRight = hitbox.pos.X + hitbox.size.X;
 
+                float playerTop = pos.Y - size.Y / 2;
+                float playerBottom = pos.Y + size.Y / 2;
+                float playerLeft = pos.X - size.X / 2;
+                float playerRight = pos.X + size.X / 2;
+
+
+                bool isColliding = playerRight > hitboxLeft && playerLeft < hitboxRight && playerBottom > hitboxTop && playerTop < hitboxBottom;
+
+                if (isColliding)
+                {
+                    if (hitbox.isBad)
+                    {
+                        //Game.titleScreen = true;
+                        Console.WriteLine("its a bad box");
+                    }
+                    if(hitbox.isBad == false)
+                    {
+                        //Game.winScreen = true;
+                        Console.WriteLine("its a good box");
+                    }
+                }
+
+            }
         }
     }
 }
